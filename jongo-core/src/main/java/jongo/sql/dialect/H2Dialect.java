ae1170866ca1c0fc7956607e7bd724d94005844d
ubersoldat <alejandroayuso@gmail.com>
2013-04-22 21:35:58 +0200

    Change class comment

--- jongo-core/src/main/java/jongo/sql/dialect/H2Dialect.java
+++ jongo-core/src/main/java/jongo/sql/dialect/H2Dialect.java
@@ -30,8 +30,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Dialect for Oracle 8g+ RDBMS.
- * @author Alejandro Ayuso 
+ * Dialect for H2 RDBMS.
+ * @author samarjit samanta
  */
 public class H2Dialect extends SQLDialect {
     
