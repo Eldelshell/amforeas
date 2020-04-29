c22df75b3b804c8a1de83419012d741e3fc9fcba
Alejandro Ayuso <alejandroayuso@gmail.com>
2020-04-29 17:02:47 +0200

    Remove syso

--- amforeas-core/src/test/java/org/amforeas/sql/dialect/OracleDialectTest.java
+++ amforeas-core/src/test/java/org/amforeas/sql/dialect/OracleDialectTest.java
@@ -49,20 +49,20 @@ public class OracleDialectTest extends SQLDialectTest {
         doTest("SELECT t.* FROM demo1.a_table t", new Select(table));
 
         doTest("SELECT t.* FROM demo1.a_table t WHERE t.tableId = ?",
-                new Select(table).setParameter(new SelectParam(table.getPrimaryKey(), "1")));
+            new Select(table).setParameter(new SelectParam(table.getPrimaryKey(), "1")));
 
         doTest("SELECT t.* FROM demo1.a_table t WHERE t.name = ?",
-                new Select(table).setParameter(new SelectParam("name", "1")));
+            new Select(table).setParameter(new SelectParam("name", "1")));
 
         doTest("SELECT t.* FROM demo1.a_table t WHERE t.tableId = ? ORDER BY t.tableId ASC", new Select(table)
-                .setParameter(new SelectParam(table.getPrimaryKey(), "1")).setOrderParam(new OrderParam(table)));
+            .setParameter(new SelectParam(table.getPrimaryKey(), "1")).setOrderParam(new OrderParam(table)));
 
         doTest("SELECT * FROM ( SELECT ROW_NUMBER() OVER ( ORDER BY t.tableId ) AS ROW_NUMBER, t.* FROM demo1.a_table t WHERE t.name = ?) WHERE ROW_NUMBER BETWEEN 0 AND 25",
-                new Select(table).setParameter(new SelectParam("name", "1")).setLimitParam(new LimitParam()));
+            new Select(table).setParameter(new SelectParam("name", "1")).setLimitParam(new LimitParam()));
 
         doTest("SELECT * FROM ( SELECT ROW_NUMBER() OVER ( ORDER BY t.name DESC ) AS ROW_NUMBER, t.* FROM demo1.a_table t WHERE t.tableId = ?) WHERE ROW_NUMBER BETWEEN 0 AND 25",
-                new Select(table).setParameter(new SelectParam(table.getPrimaryKey(), "1")).setLimitParam(l)
-                        .setOrderParam(new OrderParam("name", "DESC")));
+            new Select(table).setParameter(new SelectParam(table.getPrimaryKey(), "1")).setLimitParam(l)
+                .setOrderParam(new OrderParam("name", "DESC")));
     }
 
     @Test
@@ -76,23 +76,23 @@ public class OracleDialectTest extends SQLDialectTest {
     @Override
     public void testInsert () {
         doTest("INSERT INTO demo1.a_table (name,age) VALUES (?,?)",
-                new Insert(table).addColumn("name", "foo bar").addColumn("age", "50"));
+            new Insert(table).addColumn("name", "foo bar").addColumn("age", "50"));
     }
 
     @Test
     @Override
     public void testUpdate () {
         doTest("UPDATE demo1.a_table SET a_table.name=?,a_table.age=? WHERE a_table.tableId=?",
-                new Update(table).setId("1").addColumn("name", "foo bar").addColumn("age", "50"));
+            new Update(table).setId("1").addColumn("name", "foo bar").addColumn("age", "50"));
         doTest("UPDATE demo1.grrr SET grrr.name=? WHERE grrr.id=?",
-                new Update(new Table("demo1", "grrr")).setId("1").addColumn("name", "foo bar"));
+            new Update(new Table("demo1", "grrr")).setId("1").addColumn("name", "foo bar"));
     }
 
     @Test
     public void testDynamicFinders () {
-        System.out.println(d.toStatementString(new DynamicFinder("test", "findAllBy", "Name"), l, o));
+        //        System.out.println(d.toStatementString(new DynamicFinder("test", "findAllBy", "Name"), l, o));
         assertEquals(
-                "SELECT * FROM ( SELECT ROW_NUMBER() OVER ( ORDER BY test.tableId ASC ) AS ROW_NUMBER, test.* FROM test WHERE  name = ? ) WHERE ROW_NUMBER BETWEEN 0 AND 25",
-                d.toStatementString(new DynamicFinder("test", "findAllBy", "Name"), l, o));
+            "SELECT * FROM ( SELECT ROW_NUMBER() OVER ( ORDER BY test.tableId ASC ) AS ROW_NUMBER, test.* FROM test WHERE  name = ? ) WHERE ROW_NUMBER BETWEEN 0 AND 25",
+            d.toStatementString(new DynamicFinder("test", "findAllBy", "Name"), l, o));
     }
 }
