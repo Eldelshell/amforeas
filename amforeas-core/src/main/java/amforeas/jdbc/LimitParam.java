fe40d7d14bc14213267ed36352918308a4e09c7f
Alejandro Ayuso <alejandroayuso@gmail.com>
2020-04-30 17:25:55 +0200

    Add tests

--- amforeas-core/src/main/java/amforeas/jdbc/LimitParam.java
+++ amforeas-core/src/main/java/amforeas/jdbc/LimitParam.java
@@ -1,19 +1,13 @@
 /**
- * Copyright (C) 2011, 2012 Alejandro Ayuso
+ * Copyright (C) Alejandro Ayuso
  *
- * This file is part of Amforeas.
- * Amforeas is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * This file is part of Amforeas. Amforeas is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or any later version.
  * 
- * Amforeas is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Amforeas is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with Amforeas.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Amforeas. If not, see <http://www.gnu.org/licenses/>.
  */
 package amforeas.jdbc;
 
@@ -26,78 +20,77 @@ import org.apache.commons.lang3.StringUtils;
  * their correct form in SQL. If no limit parameter is given, the default is 25
  * the configuration. The default value for the offset or start is 0.
  * The limit value has a maximum of 1000.
- * @author Alejandro Ayuso 
  */
 public class LimitParam {
-    
+
     private final Integer limit;
     private final Integer start;
-    
+
     private static final Integer default_limit = Integer.valueOf(25);
     private static final Integer max_limit = Integer.valueOf(1000);
-    
-    public LimitParam(){
+
+    public LimitParam() {
         this.limit = default_limit;
         this.start = 0;
     }
-    
-    public LimitParam(Integer limit){
+
+    public LimitParam(Integer limit) {
         this.limit = getMaxLimit(limit);
         this.start = 0;
     }
-    
-    public LimitParam(Integer limit, Integer start){
+
+    public LimitParam(Integer limit, Integer start) {
         this.limit = getMaxLimit(limit);
         this.start = start;
     }
 
-    public Integer getLimit() {
+    public Integer getLimit () {
         return limit;
     }
 
-    public Integer getStart() {
+    public Integer getStart () {
         return start;
     }
-    
-    private Integer getMaxLimit(Integer limit){
-        if(limit >= max_limit){
+
+    private Integer getMaxLimit (Integer limit) {
+        if (limit >= max_limit) {
             limit = max_limit;
         }
         return limit;
     }
-    
+
     /**
      * From the received parameters, try to obtain a LimitParam object. By default, the LimitParam
      * always has a limit of 25 and an offset (start) in 0.
      * @param pathParams
      * @return 
      */
-    public static LimitParam valueOf(final MultivaluedMap<String, String> pathParams){
+    public static LimitParam valueOf (final MultivaluedMap<String, String> pathParams) {
         Integer l = null;
-        if(StringUtils.isNumeric(pathParams.getFirst("limit"))){
+        if (StringUtils.isNumeric(pathParams.getFirst("limit"))) {
             l = Integer.valueOf(pathParams.getFirst("limit"));
         }
-        
+
         Integer o = null;
-        if(StringUtils.isNumeric(pathParams.getFirst("offset"))){
+        if (StringUtils.isNumeric(pathParams.getFirst("offset"))) {
             o = Integer.valueOf(pathParams.getFirst("offset"));
         }
-        
+
         LimitParam instance;
-        if(l == null){
+        if (l == null) {
             instance = new LimitParam();
-        }else{
-            if(o == null){
+        } else {
+            if (o == null) {
                 instance = new LimitParam(l);
-            }else{
-                instance = new LimitParam(l,o);
+            } else {
+                instance = new LimitParam(l, o);
             }
         }
         return instance;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString () {
         StringBuilder b = new StringBuilder("{LimitParam:{limit:");
         b.append(limit);
         b.append(", start: ");
---
+++ amforeas-core/src/test/java/org/amforeas/jdbc/LimitParamTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) Alejandro Ayuso
+ *
+ * This file is part of Amforeas. Amforeas is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * Amforeas is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Amforeas. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.amforeas.jdbc;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import amforeas.jdbc.LimitParam;
+
+/**
+ * Tests for {@link amforeas.jdbc.LimitParam}
+ */
+@Tag("offline-tests")
+public class LimitParamTest {
+
+    @Test
+    public void testDefaults () {
+        MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+        assertDefault(LimitParam.valueOf(params));
+    }
+
+    @Test
+    public void testCorrect () {
+        MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+        params.add("limit", "10");
+        assertEquals(LimitParam.valueOf(params).getLimit(), 10);
+        assertEquals(LimitParam.valueOf(params).getStart(), 0);
+
+        params.add("offset", "20");
+        assertEquals(LimitParam.valueOf(params).getLimit(), 10);
+        assertEquals(LimitParam.valueOf(params).getStart(), 20);
+    }
+
+    @Test
+    public void testIncorrect () {
+        MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+        params.add("limit", "-1");
+        assertDefault(LimitParam.valueOf(params));
+
+        params.add("limit", "foo");
+        assertDefault(LimitParam.valueOf(params));
+
+        params.add("offset", "-1");
+        assertDefault(LimitParam.valueOf(params));
+
+        params.add("offset", "foo");
+        assertDefault(LimitParam.valueOf(params));
+
+        /* with an invalid limit, the offset is still the default */
+        params.add("offset", "10");
+        assertDefault(LimitParam.valueOf(params));
+
+        /* with an invalid offset, limit works and offset is default */
+        params.add("limit", "10");
+        params.add("offset", "foo");
+        assertEquals(LimitParam.valueOf(params).getLimit(), 25);
+        assertEquals(LimitParam.valueOf(params).getStart(), 0);
+
+        /* if the limit is too big */
+        params.clear();
+        params.add("limit", "1001");
+        assertEquals(LimitParam.valueOf(params).getLimit(), 1000);
+    }
+
+    private void assertDefault (LimitParam lp) {
+        assertNotNull(lp);
+        assertTrue(lp.getLimit().equals(25));
+        assertTrue(lp.getStart().equals(0));
+    }
+
+}
