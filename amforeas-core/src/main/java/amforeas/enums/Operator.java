0bb8552d3d2bb13301095f1df5c45f2935469465
Alejandro Ayuso <alejandroayuso@gmail.com>
2020-04-30 17:17:40 +0200

    Fix ILIKE string

--- amforeas-core/src/main/java/amforeas/enums/Operator.java
+++ amforeas-core/src/main/java/amforeas/enums/Operator.java
@@ -1,19 +1,13 @@
 /**
- * Copyright (C) 2011, 2012 Alejandro Ayuso
+ * Copyright (C) Alejandro Ayuso
  *
- * This file is part of Amforeas.
- * Amforeas is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * This file is part of Amforeas. Amforeas is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or any later version.
  * 
- * Amforeas is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Amforeas is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with Amforeas.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Amforeas. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package amforeas.enums;
@@ -23,7 +17,6 @@ import java.util.List;
 
 /**
  * Enum with the different SQL operators. Used by the {@link org.amforeas.jdbc.DynamicFinder}
- * @author Alejandro Ayuso 
  */
 public enum Operator {
     AND,
@@ -40,15 +33,15 @@ public enum Operator {
     NOT,
     EQUALS,
     NOTEQUALS;
-    
+
     private static final List<String> keywords = new ArrayList<String>();
-    
+
     /**
      * Returns the SQL representation of the operator.
      * @return 
      */
-    public String sql(){
-        switch(this){
+    public String sql () {
+        switch (this) {
             case AND:
                 return "AND";
             case OR:
@@ -62,8 +55,9 @@ public enum Operator {
             case GREATERTHANEQUALS:
                 return ">=";
             case LIKE:
-            case ILIKE:
                 return "LIKE";
+            case ILIKE:
+                return "ILIKE";
             case ISNOTNULL:
                 return "IS NOT NULL";
             case ISNULL:
@@ -80,13 +74,13 @@ public enum Operator {
                 throw new UnsupportedOperationException();
         }
     }
-    
+
     /**
      * Is the current operator an unary operation?
      * @return true if ISNULL or ISNOTNULL.
      */
-    public boolean isUnary(){
-        switch(this){
+    public boolean isUnary () {
+        switch (this) {
             case ISNULL:
             case ISNOTNULL:
                 return true;
@@ -94,21 +88,21 @@ public enum Operator {
                 return false;
         }
     }
-    
+
     /**
      * Is the current operator a binary operation?
      * @return 
      */
-    public boolean isBinary(){
+    public boolean isBinary () {
         return !this.isUnary() && !this.isBoolean();
     }
-    
+
     /**
      * Is AND or OR?
      * @return true for AND or OR operators.
      */
-    public boolean isBoolean(){
-        switch(this){
+    public boolean isBoolean () {
+        switch (this) {
             case AND:
             case OR:
                 return true;
@@ -116,13 +110,13 @@ public enum Operator {
                 return false;
         }
     }
-    
+
     /**
      * Returns a list with the supported SQL keywords which may render an operator.
      * @return 
      */
-    public static List<String> keywords(){
-        if(keywords.isEmpty()){
+    public static List<String> keywords () {
+        if (keywords.isEmpty()) {
             keywords.add("Is");
             keywords.add("Not");
             keywords.add("Null");
@@ -135,7 +129,7 @@ public enum Operator {
             keywords.add("Or");
             keywords.add("Like");
         }
-        
+
         return keywords;
     }
 }
