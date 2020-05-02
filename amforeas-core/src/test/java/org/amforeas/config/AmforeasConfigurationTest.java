54b4e959db590dbec124c5c3f75578675e3e8e69
Alejandro Ayuso <alejandroayuso@gmail.com>
2020-05-02 18:48:36 +0200

    Add more tests

--- amforeas-core/src/test/java/org/amforeas/config/AmforeasConfigurationTest.java
+++ amforeas-core/src/test/java/org/amforeas/config/AmforeasConfigurationTest.java
@@ -1,7 +1,11 @@
 package org.amforeas.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+import java.util.Properties;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,19 +18,66 @@ import amforeas.config.AmforeasConfiguration;
 @Tag("offline-tests")
 class AmforeasConfigurationTest {
 
+    private final Properties javaProperties = new Properties();
+
+    private AmforeasConfiguration conf;
+
     @Mock
     SingletonFactory factory;
 
-    @Test
-    void test_formatProperty () {
+    @BeforeEach
+    public void setUpEach () {
+        javaProperties.clear();
+        javaProperties.setProperty("amforeas.server.root", "/amforeas/*");
+        javaProperties.setProperty("amforeas.server.host", "0.0.0.0");
+        javaProperties.setProperty("amforeas.server.http.port", "8080");
+        javaProperties.setProperty("amforeas.alias.list", "alias1, alias2");
+        javaProperties.setProperty("amforeas.alias1.jdbc.driver", "H2_MEM");
+        javaProperties.setProperty("amforeas.alias1.jdbc.database", "test_db");
+        javaProperties.setProperty("amforeas.alias2.jdbc.driver", "MSSQL_JTDS");
+        javaProperties.setProperty("amforeas.alias2.jdbc.database", "test_db2");
+
         // This way we stop the configuration from setting up the database
-        when(factory.getConfiguration()).thenReturn(new AmforeasConfiguration());
+        when(factory.getConfiguration()).thenReturn(new AmforeasConfigurationStub());
+        conf = factory.getConfiguration();
+        conf.loadProperties();
+        conf.load();
+    }
 
-        AmforeasConfiguration conf = factory.getConfiguration();
-        // assertEquals("PROPERTY_NAME", conf.formatProperty("property.name"));
+    @Test
+    void test_getDatabaseConfigurations () {
+        assertEquals("test_db", conf.getDatabaseConfiguration("test_db").getDatabase());
+        assertNull(conf.getDatabaseConfiguration(null));
+        assertNull(conf.getDatabaseConfiguration(""));
+        assertNull(conf.getDatabaseConfiguration("invalid"));
+    }
 
-        // This should fail when we set $export AMFOREAS_SERVER_PORT=808 or -Damforeas.server.port=808
+    @Test
+    void test_getDatabaseConfigurationForAlias () {
+        assertEquals("test_db", conf.getDatabaseConfigurationForAlias("alias1").getDatabase());
+        assertThrows(IllegalArgumentException.class, () -> conf.getDatabaseConfigurationForAlias(null));
+        assertThrows(IllegalArgumentException.class, () -> conf.getDatabaseConfigurationForAlias(""));
+        assertThrows(IllegalArgumentException.class, () -> conf.getDatabaseConfigurationForAlias("invalid"));
+    }
+
+    @Test
+    void test_getters () {
         assertEquals(8080, conf.getServerPort());
+        assertEquals("/amforeas/*", conf.getServerRoot());
+        assertEquals("0.0.0.0", conf.getServerHost());
+        assertEquals(5, conf.getServerThreadsMin());
+        assertEquals(10, conf.getServerThreadsMax());
+        assertNull(conf.getSecurePort());
+        assertNull(conf.getJKSFile());
+        assertNull(conf.getJKSFilePassword());
+    }
+
+    private class AmforeasConfigurationStub extends AmforeasConfiguration {
+
+        public void loadProperties () {
+            this.properties.load(javaProperties);
+        }
+
     }
 
 }
