7458676eaff358af11307dada29b599911a5ce43
Alejandro Ayuso <alejandroayuso@gmail.com>
2020-05-02 10:20:12 +0200

    Inherit from AmforeasJetty and reuse instance methods

--- amforeas-demo/src/main/java/amforeas/demo/DemoJetty.java
+++ amforeas-demo/src/main/java/amforeas/demo/DemoJetty.java
@@ -1,19 +1,10 @@
 package amforeas.demo;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import amforeas.AmforeasJetty;
 import amforeas.SingletonFactory;
 import amforeas.config.AmforeasConfiguration;
 
-public class DemoJetty {
-
-    private static final Logger l = LoggerFactory.getLogger(AmforeasJetty.class);
+public class DemoJetty extends AmforeasJetty {
 
     public static void main (String[] args) throws Exception {
         System.out.println("*************************************************");
@@ -25,40 +16,8 @@ public class DemoJetty {
         SingletonFactory factory = new DemoSingletonFactory();
         final AmforeasConfiguration conf = factory.getConfiguration();
 
-        final Server server = new Server();
+        DemoJetty me = new DemoJetty();
+        me.startServer(conf);
 
-        setupJerseyServlet(conf, server);
-        setupHTTPConnection(conf, server);
-
-        server.start();
-        server.setStopAtShutdown(true);
-        server.join();
     }
-
-    private static void setupJerseyServlet (final AmforeasConfiguration conf, final Server server) {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.setContextPath("/");
-
-        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, conf.getServerRoot());
-        jerseyServlet.setInitOrder(0);
-        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "amforeas.rest, amforeas.filter");
-
-        server.setHandler(context);
-    }
-
-    private static void setupHTTPConnection (final AmforeasConfiguration conf, final Server server) {
-        final Integer port = conf.getServerPort();
-
-        if (port == null || port == 0) {
-            l.debug("Invalid HTTP configuration {}", port);
-            return;
-        }
-
-        l.info("Listening on HTTP port {}", port);
-        ServerConnector connector = new ServerConnector(server);
-        connector.setPort(port);
-        connector.setHost(conf.getServerHost());
-        server.addConnector(connector);
-    }
-
 }
